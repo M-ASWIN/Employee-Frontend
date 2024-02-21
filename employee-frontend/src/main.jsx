5bac00ff5e1ace668af81359982ccd4609185180
Aswin M <aswinm.cse2021@citchennai.net>
2024-02-21 18:42:09 +0530

    add project

---
+++ employee-frontend/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import EmployeeForm from './Employee.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <EmployeeForm />
+  </React.StrictMode>,
+)
