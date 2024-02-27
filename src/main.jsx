abea22414920322a2dd6a91b3a3cfcb7d5122110
Aswin M <aswinm.cse2021@citchennai.net>
2024-02-27 22:04:27 +0530

    router changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import EmployeeForm from './Employee.jsx'
-import View from './view.jsx';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { createRoot } from 'react-dom/client';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import EmployeeForm from './Employee.jsx'
+// import View from './view.jsx';
 
-function App() {
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<EmployeeForm />} />
+//         <Route path="/view" element={<View />}/>
+//       </Routes>
+//     </Router>
+//   );
+// }
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+// main.jsx
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import EmployeeForm from "./Employee";
+import view from "./view";
+
+const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<EmployeeForm />} />
-        <Route path="/view" element={<View />}/>
-      </Routes>
+      <Switch>
+        <Route exact path="/" component={EmployeeForm} />
+        <Route path="/view" component={view} />
+      </Switch>
     </Router>
   );
-}
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
