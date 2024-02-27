3e151744c5769f792b18ad80edb99bd63792c3a2
Aswin M <aswinm.cse2021@citchennai.net>
2024-02-27 22:07:30 +0530

    router changes

--- src/main.jsx
+++ src/main.jsx
@@ -24,17 +24,17 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import EmployeeForm from "./Employee";
 import view from "./view";
 
 const App = () => {
   return (
     <Router>
-      <Switch>
+      <Routes>
         <Route exact path="/" component={EmployeeForm} />
         <Route path="/view" component={view} />
-      </Switch>
+      </Routes>
     </Router>
   );
 };
