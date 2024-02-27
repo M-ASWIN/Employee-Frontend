0060f8db6fcedd4715df2514de446729f86bd371
Aswin M <aswinm.cse2021@citchennai.net>
2024-02-27 22:10:37 +0530

    route

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { createRoot } from 'react-dom/client';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import EmployeeForm from './Employee.jsx'
-// import View from './view.jsx';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<EmployeeForm />} />
-//         <Route path="/view" element={<View />}/>
-//       </Routes>
-//     </Router>
-//   );
-// }
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-// main.jsx
 
 import React from "react";
 import ReactDOM from "react-dom";
